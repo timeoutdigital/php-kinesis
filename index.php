fa526458e5a5251cc8bce2b1f6f02881be94c4a8
Tom Verran <tomverran@timeout.com>
2016-11-29 17:04:20 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+//http://docs.aws.amazon.com/aws-sdk-php/v3/api/
+
+require("vendor/autoload.php");
+use Aws\Kinesis\KinesisClient;
+
+if (count($argv) < 3) {
+  die("Usage: php-kinesis <aws region> <stream id> [event=LATEST]\n");
+}
+
+$event = $argv[3] ?? 'LATEST';
+list($region, $stream) = array_slice($argv, 1);
+
+$client = KinesisClient::factory([
+    'profile' => 'default',
+    'region'  => $region,
+    'version' => '2013-12-02'
+]);
+
+$shardId = $client->describeStream([
+  'StreamName' => $stream
+])['StreamDescription']['Shards'][0]['ShardId'];
+
+$shardIterator = $client->getShardIterator([
+  'StreamName' => $stream,
+  'ShardId' => $shardId,
+  'ShardIteratorType' => $event
+])['ShardIterator'];
+
+while(true) {
+
+  try {
+    $result = $client->getRecords(['ShardIterator' => $shardIterator]);
+  } catch (Exception $e) {
+    sleep(1);
+    continue;
+  }
+
+  if ($result['NextShardIterator']) {
+    $shardIterator = $result['NextShardIterator'];
+  }
+
+  $avro = array_map(function($r) {
+    $io = new AvroStringIO($r['Data']);
+    $reader = new AvroDataIOReader($io, new AvroIODatumReader());
+    return $reader->data()[0];
+  }, $result['Records']);
+
+  foreach($avro as $a) {
+      echo json_encode($a);
+  }
+}
